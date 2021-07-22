106a1e5757fae0b0eff6be595084fb9bedf025c1
Serhii Lytvyn <arztpsychiater@gmail.com>
2021-07-22 12:02:32 +0300

    feat: add stack navigation

--- App.tsx
+++ App.tsx
@@ -4,10 +4,34 @@ import { Provider } from 'react-redux';
 import { store } from './src/store/store';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import SearchScreen from './src/screens/SearchScreen/SearchScreen';
 import FavouritesScreen from './src/screens/FavouritesScreen/FavouritesScreen';
+import DescriptonScreen from './src/screens/DescriptonScreen/DescriptonScreen';
 import LikeImage from './assets/LikeImage';
 import SearchImage from './assets/SearchImage';
+
+const SearchStack = createStackNavigator();
+
+const SearchStackScreen = () => {
+  return (
+    <SearchStack.Navigator>
+      <FavouritesStack.Screen name="Search" component={SearchScreen} />
+      <SearchStack.Screen name="Description" component={DescriptonScreen} />
+    </SearchStack.Navigator>
+  )
+};
+
+const FavouritesStack = createStackNavigator();
+
+const FavouritesStackScreen = () => {
+  return (
+    <FavouritesStack.Navigator>
+      <SearchStack.Screen name="Favourites" component={FavouritesScreen} />
+      <FavouritesStack.Screen name="Description" component={DescriptonScreen} />
+    </FavouritesStack.Navigator>
+  )
+};
 
 const Tab = createBottomTabNavigator();
 
@@ -31,12 +55,12 @@ const App = () => {
             }
           })}
         >
-          <Tab.Screen name="Search" component={SearchScreen} />
-          <Tab.Screen name="Favourites" component={FavouritesScreen} />
+          <Tab.Screen name="Search" component={SearchStackScreen} />
+          <Tab.Screen name="Favourites" component={FavouritesStackScreen} />
         </Tab.Navigator>
       </NavigationContainer>
     </Provider>
   )
 }
 
-export default App
+export default App;
