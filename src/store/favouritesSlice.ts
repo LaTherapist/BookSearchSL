49e3965c9209b53fc3dcffb857f6bdc31e123def
Serhii Lytvyn <arztpsychiater@gmail.com>
2021-07-26 15:54:07 +0300

    refactor: change favouritesSlice structure

--- src/store/favouritesSlice.ts
+++ src/store/favouritesSlice.ts
@@ -1,17 +1,28 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {BookData} from './BookData.interface';
 
+interface InitialState {
+  favourites: {
+    [id: string]: BookData;
+  };
+}
+
+const initialState: InitialState = {
+  favourites: {},
+};
+
 const favouritesSlice = createSlice({
   name: 'favourites',
-  initialState: {
-    favourites: [{}],
-  },
+  initialState,
   reducers: {
     setFavourite(state, action: PayloadAction<BookData>) {
-      state.favourites.push(action.payload);
+      state.favourites[action.payload.id] = action.payload;
+    },
+    deleteFavourite(state, action: PayloadAction<BookData>) {
+      delete state.favourites[action.payload.id];
     },
   },
 });
 
-export const {setFavourite} = favouritesSlice.actions;
+export const {setFavourite, deleteFavourite} = favouritesSlice.actions;
 export default favouritesSlice.reducer;
