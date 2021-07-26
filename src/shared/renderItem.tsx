ccf5386a973ce8fb3b3059ea9353b5634d70f3df
Serhii Lytvyn <arztpsychiater@gmail.com>
2021-07-26 16:03:13 +0300

    refactor: rename renderItem on BookItem

--- src/shared/renderItem.tsx
+++ src/shared/renderItem.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {ListRenderItem} from 'react-native';
 import {BookData} from '../store/BookData.interface';
-import BookItem from '../screens/SearchScreen/components/renderItem/BookItem';
+import BookItem from '../screens/SearchScreen/components/BookItem/BookItem';
 
 const renderItem: ListRenderItem<BookData> = ({item}) => {
   return <BookItem item={item} />;
